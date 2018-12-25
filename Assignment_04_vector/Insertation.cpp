f4e881b7e3cfceef2c46be1906324fe3693bf8d7
soayeb95 <soayeb95@gmail.com>
2018-12-26 02:32:50 +0600

    Insertation Sort using vector

--- Assignment_04_vector/Insertation.cpp
+++ Assignment_04_vector/Insertation.cpp
@@ -9,16 +9,16 @@ void InsertionSorter(vector<int>& newMyVector);
 int main()
 {
 
-	vector<int>myVector{ 10,33,27,14,35,19,48,44 };
-	InsertionSorter(myVector);
+    vector<int>myVector{10,33,27,14,35,19,48,44};
+    InsertionSorter(myVector);
 }
 
 void InsertionSorter(vector<int>& newMyVector)
 {
-	if (newMyVector.size() > 1)
+	if (newMyVector.size()> 1)
 	{
 		int size = newMyVector.size();
-		for (int i  = 1; i < size; ++i)
+		for (int i = 1; i < size; ++i)
 		{
 			int j = i - 1;
 			int key = newMyVector[i];
@@ -28,7 +28,7 @@ void InsertionSorter(vector<int>& newMyVector)
 				--j;
 			}
 			newMyVector[j + 1] = key;
-			for (int k = 0;k < size;k++) cout << newMyVector[k] << " ";
+			for (int k = 0;k <size;k++) cout << newMyVector[k] << " ";
 			cout << endl;
 		}
 	}
